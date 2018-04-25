ea64464f9ccf296e1d7ccc9ecb91a533e4c5fe09
azrafe7 <azrafe7@gmail.com>
2018-04-25 20:31:36 +0200

    separate InsertionSort

---
+++ src/InsertionSort.hx
@@ -0,0 +1,24 @@
+
+class InsertionSort 
+{
+
+  inline static public function sort<T>(a:Array<T>, cmp:T -> T -> Int):Void
+  {
+    insertionSort(a, cmp, 0, a.length);
+  }
+  
+  inline static public function insertionSort<T>(a:Array<T>, cmp:T -> T -> Int, start:Int, count:Int):Void
+  {
+    var j = start;
+    for (i in start + 1...start + count) {
+      j = i;
+      while (j > start) {
+        if (cmp(a[j], a[j - 1]) < 0)
+          Util.swap(a, j - 1, j);
+        else
+          break;
+        j--;
+      }
+    }
+  }  
+}
