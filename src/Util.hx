458bc8e7bbe0ebea04c91443bdcda3a1c2ef959a
azrafe7 <azrafe7@gmail.com>
2018-04-25 20:32:54 +0200

    Util.sortInfo()

--- src/Util.hx
+++ src/Util.hx
@@ -21,4 +21,28 @@ class Util
   inline static public function indirectCompare<T>(a:Array<T>, cmp:T -> T -> Int, i:Int, j:Int, indices:Array<Int>):Int {
     return cmp(a[indices[i]], a[indices[j]]);
   }
+  
+  static public function sortInfo<T>(a:Array<T>, cmp:T -> T -> Int) {
+    
+    inline function sign(x) {
+      return x < 0 ? -1 : x > 0 ? 1 : 0;
+    }
+    
+    var sorted = true;
+    var invSorted = true;
+    var allSame = true;
+    
+    var diff = sign(cmp(a[0], a[1]));
+    for (i in 1...a.length) {
+      diff = sign(cmp(a[i - 1], a[i]));
+      
+      if (sorted && diff > 0) sorted = false;
+      if (invSorted && diff < 0) invSorted = false;
+      if (allSame && diff != 0) allSame = false;
+      
+      if (!sorted && !invSorted && !allSame) break;
+    }
+    
+    return {sorted:sorted, invSorted:invSorted, allSame:allSame};
+  }
 }
