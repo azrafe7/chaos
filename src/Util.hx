217ff641ffdb9ade9716f6b78a535db36b523299
azrafe7 <azrafe7@gmail.com>
2018-04-25 20:24:06 +0200

    Util.copy() and swapCounts

--- src/Util.hx
+++ src/Util.hx
@@ -2,11 +2,17 @@ package;
 
 class Util 
 {
+  static public var swapCount = 0;
 	inline static public function swap<T>(a:Array<T>, i:Int, j:Int):Void {
-		var tmp = a[i];
+		swapCount++;
+    var tmp = a[i];
 		a[i] = a[j];
 		a[j] = tmp;
 	}
+  
+  inline static public function copy<T>(src:Array<T>, dest:Array<T>, count:Int):Void {
+    for (i in 0...count) dest[i] = src[i];
+  }
   
   inline static public function compare<T>(a:Array<T>, cmp:T -> T -> Int, i:Int, j:Int):Int {
     return cmp(a[i], a[j]);
