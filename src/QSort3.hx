126aee6f1410eebf7f1e5f29b1a3e4f4abd5233f
azrafe7 <azrafe7@gmail.com>
2018-04-30 23:16:53 +0200

    QSort3: move partition code to its own function

--- src/QSort3.hx
+++ src/QSort3.hx
@@ -16,6 +16,8 @@ class QSort3
     QuickSort.stackDepth = level > QuickSort.stackDepth ? level : QuickSort.stackDepth;
     QuickSort.calls++;
     
+    var out_ij = [0, 0];
+    
     while (lo < hi) {
       
       // OPT: use insertion sort for small sequences
@@ -33,46 +35,12 @@ class QSort3
         return;
       }
       
-      var i = lo - 1, j = hi, p = lo - 1, q = hi; 
-      var pivot = a[hi];
-      
-      while (true)
-      {
-        while (cmp(a[++i], pivot) < 0) {};
-        while (cmp(pivot, a[--j]) < 0) {
-          if (j == lo) break;
-        }
-        if (i >= j) break;
-        Util.swap(a, i, j);
-        
-        if (cmp(a[i], pivot) == 0) {
-          p++; 
-          Util.swap(a, p, i); 
-        }
-        if (cmp(pivot, a[j]) == 0) {
-          q--; 
-          Util.swap(a, j, q); 
-        }
-      }
-      Util.swap(a, i, hi); 
-      j = i - 1;
-      i = i + 1;
-      
-      var k = lo;
-      while (k < p) {
-        Util.swap(a, k, j);
-        k++;
-        j--;
-      }
-      k = hi - 1;
-      while (k > q) {
-        Util.swap(a, i, k);
-        k--;
-        i++;
-      }
+      partition(a, cmp, lo, hi, out_ij);
+      var i = out_ij[0];
+      var j = out_ij[1];
       
       // OPT: sort smaller sequence first and update the bounds afterwards.
-      //      Helps in keeping stack depth to a minimum, avoids creating unnecessary stackframes while recursing.
+      //      Helps in keeping stack depth to a minimum by not creating unnecessary stackframes while recursing.
       if (j - lo < hi - i) {
         qsort(a, cmp, lo, j, level + 1);
         lo = i;
@@ -81,5 +49,55 @@ class QSort3
         hi = j;
       }
     }
+  }
+  
+  static public function partition<T>(a:Array<T>, cmp:T -> T -> Int, lo:Int, hi:Int, out_ij:Array<Int>):Void
+  {
+    var i = lo - 1, j = hi, p = lo - 1, q = hi;
+    var pivot = a[hi];
+    
+    while (true)
+    {
+      while (cmp(a[++i], pivot) < 0) {};
+      while (cmp(pivot, a[--j]) < 0) {
+        if (j == lo) break;
+      }
+      if (i >= j) break;
+      Util.swap(a, i, j);
+      
+      if (cmp(a[i], pivot) == 0) {
+        p++; 
+        Util.swap(a, p, i); 
+      }
+      if (cmp(pivot, a[j]) == 0) {
+        q--; 
+        Util.swap(a, j, q); 
+      }
+    }
+    Util.swap(a, i, hi); 
+    //trace(a);
+    //trace(Util.highlightIndices(a, [lo, hi], ['L','H']));
+    //trace(Util.highlightIndices(a, [i, j], ['i','j']));
+    //trace(Util.highlightIndices(a, [p, q], ['p','q']));
+    j = i - 1;
+    i = i + 1;
+    
+    var k = lo;
+    while (k < p) {
+      Util.swap(a, k, j);
+      k++;
+      j--;
+    }
+    k = hi - 1;
+    while (k > q) {
+      Util.swap(a, i, k);
+      k--;
+      i++;
+    }
+    //trace('pivot in place $pivot');
+    //trace(a);
+    
+    out_ij[0] = i;
+    out_ij[1] = j;
   }
 }
