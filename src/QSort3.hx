2df1dd5d508d37c4cd8fe21a9075a28a8bb1f118
azrafe7 <azrafe7@gmail.com>
2018-05-01 21:50:12 +0200

    instrumented code for exploration (also check k >= q)

--- src/QSort3.hx
+++ src/QSort3.hx
@@ -4,9 +4,15 @@
 
 class QSort3 
 {
+
+  static public function assert(cond:Bool, msg = "error")
+  {
+    if (!cond) throw msg;
+  }
+  
   inline static public function sort<T>(a:Array<T>, cmp:T -> T -> Int):Void
   {
-    Util.shuffle(a); // check if needed to make it more stable/predictable, or can be removed
+    //Util.shuffle(a); // check if needed to make it more stable/predictable, or can be removed
     qsort(a, cmp, 0, a.length - 1);
   }
   
@@ -15,10 +21,10 @@ class QSort3
   {
     QuickSort.stackDepth = level > QuickSort.stackDepth ? level : QuickSort.stackDepth;
     QuickSort.calls++;
-    
+    trace("Level: " + level);
     var out_ij = [0, 0];
     
-    while (lo < hi) {
+    if (lo < hi) {
       
       // OPT: use insertion sort for small sequences
       if (hi - lo < QuickSort.M) {
@@ -35,67 +41,117 @@ class QSort3
         return;
       }
       
+      trace(a.toString());
       partition(a, cmp, lo, hi, out_ij);
       var i = out_ij[0];
       var j = out_ij[1];
       
+      // test invariants
+      trace(a.toString());
+      trace(Util.highlightIndices(a, [j+1], ['^']));
+      trace(Util.highlightIndices(a, [lo, hi], ['L','H']));
+      trace(Util.highlightIndices(a, [i, j], ['i','j']));
+      
+      var pivot = a[j + 1];
+      trace("pivotValue: " + pivot);
+      
+      var ii = lo + 1;
+      while (ii < j)
+      {
+        var val = a[ii - 1];
+        assert(cmp(val, pivot) < 0, 'LT error a[${ii-1}] = $val vs $pivot');
+        ii++;
+      }
+      
+      ii = i + 1;
+      while (ii <= hi + 1) {
+        var val = a[ii - 1];
+        assert(cmp(val, pivot) > 0, 'GT error a[${ii-1}] = $val vs $pivot');
+        ii++;
+      }
+      
       // OPT: sort smaller sequence first and update the bounds afterwards.
       //      Helps in keeping stack depth to a minimum by not creating unnecessary stackframes while recursing.
-      if (j - lo < hi - i) {
+      //if (j - lo < hi - i) {
+        trace("qsort left");
         qsort(a, cmp, lo, j, level + 1);
-        lo = i;
-      } else {
+      //  lo = i;
+      //} else {
+        trace("qsort right");
         qsort(a, cmp, i, hi, level + 1);
-        hi = j;
-      }
+      //  hi = j;
+      //}
     }
   }
   
+  // 3-way partition array `a` into [lo...out_ij[1]] < pivot | [out_ij[1]+1...out_ij[0]) == pivot | [out_ij[0]...hi] > pivot
   static public function partition<T>(a:Array<T>, cmp:T -> T -> Int, lo:Int, hi:Int, out_ij:Array<Int>):Void
   {
     var i = lo - 1, j = hi, p = lo - 1, q = hi;
     var pivot = a[hi];
+    trace(a.toString());
+    trace(Util.highlightIndices(a, [hi], null));
     
     while (true)
     {
-      while (cmp(a[++i], pivot) < 0) {};
+      trace(Util.highlightIndices(a, [i,j], ['i','j']));
+      while (cmp(a[++i], pivot) < 0) {
+        trace(Util.highlightIndices(a, [i], ['i']));
+      };
       while (cmp(pivot, a[--j]) < 0) {
+        trace(Util.highlightIndices(a, [j], ['j']));
         if (j == lo) break;
       }
+      trace(a.toString());
+      trace(Util.highlightIndices(a, [i,j], ['i','j']));
       if (i >= j) break;
       Util.swap(a, i, j);
+      trace(a.toString());
+      trace(Util.highlightIndices(a, [i,j], ['i','j']));
       
       if (cmp(a[i], pivot) == 0) {
         p++; 
         Util.swap(a, p, i); 
       }
+      trace(a.toString());
+      trace(Util.highlightIndices(a, [p,q], ['p','q']));
       if (cmp(pivot, a[j]) == 0) {
         q--; 
         Util.swap(a, j, q); 
       }
+      trace(a.toString());
+      trace(Util.highlightIndices(a, [p,q], ['p','q']));
     }
     Util.swap(a, i, hi); 
-    //trace(a);
-    //trace(Util.highlightIndices(a, [lo, hi], ['L','H']));
-    //trace(Util.highlightIndices(a, [i, j], ['i','j']));
-    //trace(Util.highlightIndices(a, [p, q], ['p','q']));
+    trace(a.toString());
+    trace(Util.highlightIndices(a, [lo, hi], ['L','H']));
+    trace(Util.highlightIndices(a, [i, j], ['i','j']));
+    trace(Util.highlightIndices(a, [p, q], ['p','q']));
     j = i - 1;
     i = i + 1;
     
     var k = lo;
+    trace(a.toString());
+    trace(Util.highlightIndices(a, [k, j, p], ['k','j', 'p']));
     while (k < p) {
+      trace(a.toString());
+      trace(Util.highlightIndices(a, [k, j, p], ['k','j', 'p']));
       Util.swap(a, k, j);
       k++;
       j--;
     }
     k = hi - 1;
-    while (k > q) {
+    trace(Util.highlightIndices(a, [k, i, q], ['k','i', 'q']));
+    while (k >= q) {
+      trace(a.toString());
+      trace(Util.highlightIndices(a, [k, i, q], ['k','i', 'q']));
       Util.swap(a, i, k);
       k--;
       i++;
     }
-    //trace('pivot in place $pivot');
-    //trace(a);
+    trace('pivot in place $pivot');
+    trace(a.toString());
+    trace(Util.highlightIndices(a, [i, j], ['i', 'j']));
     
     out_ij[0] = i;
     out_ij[1] = j;
