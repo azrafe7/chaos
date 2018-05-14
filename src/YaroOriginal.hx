8bcd96c1727d845376cd13064e356140dc95c16e
azrafe7 <azrafe7@gmail.com>
2018-05-15 00:10:54 +0200

    YaroOriginal fixed!

--- src/YaroOriginal.hx
+++ src/YaroOriginal.hx
@@ -24,7 +24,11 @@ class YaroOriginal
     var len = hi - lo;
     var x:T;
     
+    //trace(a.toString());
+    //trace(Util.highlightIndices(a, [lo, hi], ['L', 'H']));
+    
     if (len < TINY_SIZE) { // insertion sort on tiny array
+      //trace('(use ins_sort');
       for (i in lo + 1...hi + 1) {
         var j = i;
         while (j > lo && cmp(a[j], a[j - 1]) < 0) {
@@ -35,6 +39,9 @@ class YaroOriginal
           j--;
         }
       }
+      //trace(a.toString());
+      //trace(Util.highlightIndices(a, [lo, hi], ['L', 'H']));
+      //trace('useD ins_sort)');
       return;
     }
     
@@ -47,6 +54,9 @@ class YaroOriginal
     var m5 = m4 + sixth;
     
     // 5-element sorting network
+    //trace('<5-elms');
+    //trace(a.toString());
+    //trace(Util.highlightIndices(a, [m1, m2, m3, m4, m5], '12345'));
     if (cmp(a[m1], a[m2]) > 0) { x = a[m1]; a[m1] = a[m2]; a[m2] = x; }
     if (cmp(a[m4], a[m5]) > 0) { x = a[m4]; a[m4] = a[m5]; a[m5] = x; }
     if (cmp(a[m1], a[m3]) > 0) { x = a[m1]; a[m1] = a[m3]; a[m3] = x; }
@@ -57,20 +67,33 @@ class YaroOriginal
     if (cmp(a[m2], a[m3]) > 0) { x = a[m2]; a[m2] = a[m3]; a[m3] = x; }
     if (cmp(a[m4], a[m5]) > 0) { x = a[m4]; a[m4] = a[m5]; a[m5] = x; }
     
+    //trace(a.toString());
+    //trace(Util.highlightIndices(a, [m1, m2, m3, m4, m5], '12345'));
+    //trace('5-elms>');
+    
     // pivots: [ < pivot1 | pivot1 <= && <= pivot2 | > pivot2 ]
     var pivot1 = a[m2];
     var pivot2 = a[m4];
-    var diffPivots:Bool = pivot1 != pivot2;
+    var diffPivots:Bool = (cmp(pivot1, pivot2) != 0);
     a[m2] = a[lo];
     a[m4] = a[hi];
+    
+    //trace(a.toString());
+    //trace(Util.highlightIndices(a, [lo, m2, m4, hi], 'LpqH'));
     
     // center part pointers
     var less = lo + 1;
     var great = hi - 1;
+    //trace(Util.highlightIndices(a, [less, great], '<>'));
     
     // sorting
+    //trace('sort');
+    var k = -1;
     if (diffPivots) {
-      for (k in less...great + 1) {
+      k = less;
+      while (k <= great) {
+        //trace(a.toString());
+        //trace(Util.highlightIndices(a, [less, k, great], 'lkg'));
         x = a[k];
         if (cmp(x, pivot1) < 0) {
           a[k] = a[less];
@@ -88,12 +111,18 @@ class YaroOriginal
             a[less++] = x;
           }
         }
+        
+        k++;
       }
     }
     else {
-      for (k in less...great + 1) {
+      k = less;
+      while (k <= great) {
+        //trace(a.toString());
+        //trace(Util.highlightIndices(a, [less, k, great], 'lkg'));
         x = a[k];
         if (cmp(x, pivot1) == 0) {
+          k++;
           continue;
         }
         if (cmp(x, pivot1) < 0) {
@@ -112,22 +141,31 @@ class YaroOriginal
             a[less++] = x;
           }
         }
+        
+        k++;
       }
     }
     
     // swap
+    //trace('<swap');
+    //trace(a.toString());
+    //trace(Util.highlightIndices(a, [lo, less-1, hi, great+1], 'LlHg'));
     a[lo] = a[less - 1];
     a[less - 1] = pivot1;
     a[hi] = a[great + 1];
     a[great + 1] = pivot2;
+    //trace(a.toString());
+    //trace(Util.highlightIndices(a, [lo, less-1, hi, great+1], 'LlHg'));
+    //trace('swap>');
     
     // left and right parts
     dualsort(a, cmp, lo, less - 2, level + 1);
     dualsort(a, cmp, great + 2, hi, level + 1);
     
     // equal elements
-    if (great - less > len - DIST_SIZE && diffPivots) {
-      for (k in less...great + 1) {
+    if ((great - less > len - DIST_SIZE) && diffPivots) {
+      k = less;
+      while (k <= great) {
         x = a[k];
         if (cmp(x, pivot1) == 0) {
           a[k] = a[less];
@@ -142,10 +180,15 @@ class YaroOriginal
             a[less++] = x;
           }
         }
+        
+        k++;
       }
     }
     
     // center part
+    //trace('center');
+    //trace(a.toString());
+    //trace(Util.highlightIndices(a, [less, great], 'cC'));
     if (diffPivots) {
       dualsort(a, cmp, less, great, level + 1);
     }
