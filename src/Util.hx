c4c8d4e816f262e18bf852ffb5c1aefdb110824e
azrafe7 <azrafe7@gmail.com>
2019-01-09 22:36:41 +0100

    [Util] fix Fisher-Yates shuffle

--- src/Util.hx
+++ src/Util.hx
@@ -1,75 +1,75 @@
 package;
 import haxe.ds.Either;
 
-class Util 
+class Util
 {
   static public var swapCount = 0;
-	inline static public function swap<T>(a:Array<T>, i:Int, j:Int):Void {
-		swapCount++;
+  inline static public function swap<T>(a:Array<T>, i:Int, j:Int):Void {
+    swapCount++;
     var tmp = a[i];
-		a[i] = a[j];
-		a[j] = tmp;
-	}
-  
+    a[i] = a[j];
+    a[j] = tmp;
+  }
+
   inline static public function copy<T>(src:Array<T>, dest:Array<T>, count:Int):Void {
     for (i in 0...count) dest[i] = src[i];
   }
-  
+
   inline static public function compare<T>(a:Array<T>, cmp:T -> T -> Int, i:Int, j:Int):Int {
     return cmp(a[i], a[j]);
   }
-  
+
   inline static public function indirectCompare<T>(a:Array<T>, cmp:T -> T -> Int, i:Int, j:Int, indices:Array<Int>):Int {
     return cmp(a[indices[i]], a[indices[j]]);
   }
-  
+
   static public function sortInfo<T>(a:Array<T>, cmp:T -> T -> Int) {
-    
+
     inline function sign(x) {
       return x < 0 ? -1 : x > 0 ? 1 : 0;
     }
-    
+
     var sorted = true;
     var invSorted = true;
     var allSame = true;
-    
+
     var diff = sign(cmp(a[0], a[1]));
     for (i in 1...a.length) {
       diff = sign(cmp(a[i - 1], a[i]));
-      
+
       if (sorted && diff > 0) sorted = false;
       if (invSorted && diff < 0) invSorted = false;
       if (allSame && diff != 0) allSame = false;
-      
+
       if (!sorted && !invSorted && !allSame) break;
     }
-    
+
     return {sorted:sorted, invSorted:invSorted, allSame:allSame};
   }
-  
+
   static public function shuffle<T>(a:Array<T>):Void
   {
     var len = a.length;
-    for (i in 0...len - 2) {
+    for (i in 0...len - 1) {
       var j = i + Std.random(len - i);
       Util.swap(a, i, j);
     }
   }
-  
+
   static public function shuffleRange<T>(a:Array<T>, lo:Int, hi:Int):Void
   {
     var len = hi - lo;
-    for (i in lo...lo + len - 2) {
+    for (i in 0...len - 1) {
       var j = i + Std.random(len - i);
-      Util.swap(a, i, j);
+      Util.swap(a, lo + i, lo + j);
     }
   }
-  
+
   static public function highlightIndices<T>(a:Array<T>, indices:Array<Int>, labelsStringOrArray:OneOf<String, Array<String>>)
   {
     // clean/extend labels to be same length as indices
     var labels:Array<String> = [];
-    switch (labelsStringOrArray) 
+    switch (labelsStringOrArray)
     {
       case Left(s): labels = s.split('');
       case Right(a): labels = a;
@@ -80,7 +80,7 @@ class Util
       if (i >= labels.length) labels[i] = '^';
       if (labels[i].length > 1) labels[i] = labels[i].charAt(0);
     }
-    
+
   #if js
     var str = "";
   #else
@@ -99,20 +99,20 @@ class Util
     }
     return str;
   }
-  
+
   // clamp `x` to [`min`...`max`]. (both inclusive)
   static public function iclamp(x:Int, min:Int, max:Int):Int {
     return x < min ? min : x > max ? max : x;
   }
-  
+
   static public function imin(a:Int, b:Int):Int {
     return a < b ? a : b;
   }
-  
+
   static public function imax(a:Int, b:Int):Int {
     return a > b ? a : b;
   }
-  
+
   inline static public function sign(x:Int):Int {
     return x < 0 ? -1 : x > 0 ? 1 : 0;
   }
@@ -123,11 +123,11 @@ abstract OneOf<A, B>(Either<A, B>) from Either<A, B> to Either<A, B> {
     return Left(a);
   }
   @:from inline static function fromB<A, B>(b:B):OneOf<A, B> {
-    return Right(b);  
-  } 
-    
+    return Right(b);
+  }
+
   @:to inline function toA():Null<A> return switch(this) {
-    case Left(a): a; 
+    case Left(a): a;
     default: null;
   }
   @:to inline function toB():Null<B> return switch(this) {
